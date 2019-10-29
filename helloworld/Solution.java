cde65754777254f1ba224dd3cef12cbcfd30df88
Sagar_Bilwal <32810679+Sagar-Bilwal@users.noreply.github.com>
2019-10-29 15:17:25 +0530

    Add files via upload

---
+++ helloworld/Solution.java
@@ -0,0 +1,13 @@
+ class Base {
+    public void show() {
+        System.out.println("Base show");
+        int x=1;
+    }
+}
+public class Solution {
+    public static void main(String[] args) {
+        Base b = new Base();
+        b.show();
+    }
+}
+
